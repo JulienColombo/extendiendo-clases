ea43980b18462760c001a77392a04ef4629677e5
Julien Colombo <juli-colombo@hotmail.com>
2021-06-08 12:10:04 -0300

    Cambios para que pase el test

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove"
+import * as orderBy from "lodash/orderBy"
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,29 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string){
+    super(name)
+    const productsJson = fs.readFileSync(__dirname + "/products.json")
+    .toString()
+    const productsParseados = JSON.parse(productsJson);
+    productsParseados.forEach(element => {
+      this.addProduct(element);
+    });
+  }
+  addProduct(product: Product){
+    this.add(product)
+  }
+  getProduct(id:number){
+    const cosas = this.getCosas();
+    return cosas.find((c) => (c.id == id));
+  }
+  removeProduct(id:number){
+   remove(this.cosas, (c) => c.id == id) 
+  }
+  getSortedByPrice(order:"asc" | "desc"){
+  return orderBy(this.cosas, ["price"], [order])    
+  }
+}
 
 export { ListaDeProductos, Product };
